4c89480b698440e3d4983f96d7838d75049f0f63
wish9 <wjwee9@gmail.com>
2023-04-30 16:29:22 +0900

    feat : 예외처리 Skeleton

---
+++ server/edusync/src/main/java/com/codestates/edusync/exception/ExceptionCode.java
@@ -0,0 +1,21 @@
+package com.codestates.edusync.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+
+    DUPLICATED_EMAIL(409, "이메일을 찾을수 없습니다."),
+    MEMBER_EXISTS(409, "이메일이 이미 존재"),
+    MEMBER_NOT_FOUND(404, "멤버를 찾을수 없습니다."),
+    INTERNAL_SERVER_ERROR(500, "내부 서버 오류"),
+    INVALID_PERMISSION(403, "권한이 유효하지 않습니다.");
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ server/edusync/src/main/java/com/codestates/edusync/exception/Response/Response.java
@@ -0,0 +1,24 @@
+package com.codestates.edusync.exception.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Response<T> {
+
+    private String resultCode;
+    private T result;
+
+    public static <T> Response<Void> error(String errorCode) {
+        return new Response<>(errorCode, null);
+    }
+
+    public static Response<Void> success() {
+        return new Response<Void>("SUCCESS", null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response<>("SUCCESS", result);
+    }
+}
