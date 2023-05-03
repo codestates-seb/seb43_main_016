8e7219dd2a15c108d51d9081392f37e1fbeed2da
wish9 <wjwee9@gmail.com>
2023-05-04 04:13:18 +0900

    fix : 잘못된 토큰일 경우 401 반환하도록 수정 (#58)
    (+오류 메세지에 원인 알려주도록 명시)

--- server/edusync/src/main/java/com/codestates/edusync/audit/utils/ErrorResponder.java
+++ server/edusync/src/main/java/com/codestates/edusync/audit/utils/ErrorResponder.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ErrorResponder {
-    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status) throws IOException {
+    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status, String message) throws IOException {
         Gson gson = new Gson();
-        ErrorResponse errorResponse = ErrorResponse.of(status);
+        ErrorResponse errorResponse = ErrorResponse.of(status, message);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE); // 응답의 컨텐츠 타입을 JSON으로 설정
         response.setStatus(status.value()); // status 작성
         response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class)); // response body 부분 작성
