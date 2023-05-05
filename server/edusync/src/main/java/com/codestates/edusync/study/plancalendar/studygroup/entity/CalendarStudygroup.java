f2a246ba0817e101179be0e5ca740bf2ffb9d7ff
serveman <120380939+serveman@users.noreply.github.com>
2023-05-05 20:49:12 +0900

    feat: CalendarStudygroup 추가 - 스터디그룹에서는 개인의 스케쥴을 변경할 수 있음

---
+++ server/edusync/src/main/java/com/codestates/edusync/study/plancalendar/studygroup/entity/CalendarStudygroup.java
@@ -0,0 +1,40 @@
+package com.codestates.edusync.study.plancalendar.studygroup.entity;
+
+import com.codestates.edusync.study.classmate.entity.Classmate;
+import com.codestates.edusync.study.plancalendar.classmate.entity.CalendarClassmate;
+import com.codestates.edusync.study.plancalendar.timeschedule.entity.TimeSchedule;
+import com.codestates.edusync.study.studygroup.entity.Studygroup;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class CalendarStudygroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = {PERSIST, MERGE}, fetch = LAZY)
+    @JoinColumn(name = "fk_studygroup_id")
+    private Studygroup studygroup;
+
+    // Studygroup 의 캘린더를 변경하거나 추가, 삭제할 경우, Classmate 의 캘린더에도 함께 적용한다
+    @OneToMany(cascade = {PERSIST, MERGE, REMOVE}, fetch = LAZY)
+    @JoinColumn(name = "fk_calendar_classmate_id")
+    private List<CalendarClassmate> calendarClassmates;
+
+    @OneToMany(cascade = {PERSIST, MERGE, REMOVE}, fetch = EAGER)
+    @JoinColumn(name = "fk_time_schedules_id")
+    private List<TimeSchedule> timeSchedules = new ArrayList<>();
+}
