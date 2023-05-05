bbf7c9af64716eee93fc99e02d7c6fe482af0de1
serveman <120380939+serveman@users.noreply.github.com>
2023-05-06 02:29:36 +0900

    feat: CalendarStudygroupResponseDto 추가

---
+++ server/edusync/src/main/java/com/codestates/edusync/study/plancalendar/studygroup/dto/CalendarStudygroupResponseDto.java
@@ -0,0 +1,17 @@
+package com.codestates.edusync.study.plancalendar.studygroup.dto;
+
+import com.codestates.edusync.study.plancalendar.timeschedule.dto.TimeScheduleResponseDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CalendarStudygroupResponseDto {
+    private Long id;
+
+    private List<TimeScheduleResponseDto> timeSchedules;
+}
