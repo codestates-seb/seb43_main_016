2e00b4c6790024d2a041e4f637abd8864569dea9
serveman <120380939+serveman@users.noreply.github.com>
2023-05-06 20:41:39 +0900

    feat: Studygroup Post에서 Setter 제거, Patch에 id 추가

---
+++ server/edusync/src/main/java/com/codestates/edusync/study/postcomment/dto/StudyPostCommentDto.java
@@ -0,0 +1,15 @@
+package com.codestates.edusync.study.postcomment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class StudyPostCommentDto {
+
+    public static class Post {
+
+    }
+}
