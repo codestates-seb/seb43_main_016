04d2b3c37aa0c9d1f473b435cdda39dbdbcd5364
serveman <120380939+serveman@users.noreply.github.com>
2023-05-08 17:18:23 +0900

    feat: SearchTagController 틀 추가

---
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/controller/SearchTagController.java
@@ -0,0 +1,42 @@
+package com.codestates.edusync.searchtag.controller;
+
+import com.codestates.edusync.searchtag.mapper.SearchTagMapper;
+import com.codestates.edusync.searchtag.service.SearchTagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Validated
+@RestController
+public class SearchTagController {
+    private final SearchTagService searchTagService;
+    private final SearchTagMapper mapper;
+
+    private static final String DEFAULT_SEARCH_TAG_URL = "/search";
+
+    /**
+     * key(카테고리) 와 value(태그) 의 쌍을 응답한다.
+     * @return
+     */
+    @GetMapping(DEFAULT_SEARCH_TAG_URL + "/all")
+    public ResponseEntity getSearchTags() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * key(카테고리)를 요청받아서 value(태그)들만 리턴한다.
+     * @param key
+     * @return
+     */
+    @GetMapping(DEFAULT_SEARCH_TAG_URL)
+    public ResponseEntity getSearchTags(@RequestParam("key") String key) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/mapper/SearchTagMapper.java
@@ -0,0 +1,8 @@
+package com.codestates.edusync.searchtag.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SearchTagMapper {
+
+}
