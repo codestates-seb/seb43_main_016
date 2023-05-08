e9dc87208f74667eec8a209b053b4aafb4a12e2b
serveman <120380939+serveman@users.noreply.github.com>
2023-05-08 16:49:50 +0900

    feat: ClassmateController 틀 추가

---
+++ server/edusync/src/main/java/com/codestates/edusync/study/classmate/mapper/ClassmateMapper.java
@@ -0,0 +1,8 @@
+package com.codestates.edusync.study.classmate.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ClassmateMapper {
+
+}
---
+++ server/edusync/src/main/java/com/codestates/edusync/study/classmate/service/ClassmateService.java
@@ -0,0 +1,8 @@
+package com.codestates.edusync.study.classmate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassmateService {
+
+}
