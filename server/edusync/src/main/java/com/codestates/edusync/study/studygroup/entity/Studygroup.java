b6330e0adb78835e0d8938311a808211a3f28265
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 04:08:43 +0900

    fix: Studygroup 캘린더 삭제

--- server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
@@ -1,11 +1,8 @@
 package com.codestates.edusync.study.studygroup.entity;
 
 import com.codestates.edusync.audit.Auditable;
-import com.codestates.edusync.infodto.calendarinfo.entity.CalendarInfo;
-import com.codestates.edusync.infodto.locationinfo.entity.LocationInfo;
 import com.codestates.edusync.infodto.timeschedule.entity.TimeSchedule;
 import com.codestates.edusync.member.entity.Member;
-import com.codestates.edusync.study.plancalendar.studygroup.entity.CalendarStudygroup;
 import com.codestates.edusync.study.postcomment.entity.StudygroupPostComment;
 import com.codestates.edusync.study.studygroupjoin.entity.StudygroupJoin;
 import com.codestates.edusync.searchtag.entity.SearchTag;
@@ -70,9 +67,6 @@ public class Studygroup extends Auditable {
 
     @OneToMany(mappedBy = "studygroup", cascade = {REMOVE}, fetch = LAZY)
     private List<StudygroupPostComment> studygroupPostComments = new ArrayList<>();
-
-    @OneToOne(mappedBy = "studygroup", cascade = {PERSIST, MERGE, REMOVE}, fetch = EAGER)
-    private CalendarStudygroup calendarStudygroup;
 
     @OneToMany(mappedBy = "studygroup", cascade = {PERSIST, MERGE, REMOVE}, fetch = LAZY)
     private List<SearchTag> tags = new ArrayList<>();
