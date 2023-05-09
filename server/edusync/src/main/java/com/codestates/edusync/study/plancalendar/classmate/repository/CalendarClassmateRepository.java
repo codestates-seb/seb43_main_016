761d6c55fbbd648608b46f5a6c0e9d663d2ea7be
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 04:12:29 +0900

    remove: Calendar 엔티티 삭제

--- server/edusync/src/main/java/com/codestates/edusync/study/plancalendar/classmate/repository/CalendarClassmateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.edusync.study.plancalendar.classmate.repository;
-
-import com.codestates.edusync.study.plancalendar.classmate.entity.CalendarClassmate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalendarClassmateRepository extends JpaRepository<CalendarClassmate, Long> {
-
-}
