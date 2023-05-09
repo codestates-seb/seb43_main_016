7ce38af91b599acf52c1f267ab5d939e5ed3e26f
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 02:46:15 +0900

    feat: 세번째 버전 스키마 (대폭 다이어트한 버전)

--- server/edusync/src/main/resources/schema.sql
+++ server/edusync/src/main/resources/schema.sql
@@ -1,134 +1,126 @@
-create table member (
-    id               bigint auto_increment,
-    nick_name        varchar(100) not null,
-    profile_image    blob null,
-    password         varchar(100) not null,
-    grade            varchar(50),
-    roles            varchar(50),
-    about_me         TEXT,
-    with_me          TEXT,
-    member_status    varchar(50),
-    modified_at      timestamp,
-    created_at       timestamp,
+create table if not exists member
+(
+    id            bigint auto_increment,
+    email         varchar(100),
+    nick_name     varchar(100),
+    profile_image blob,
+    password      varchar(100),
+    address       varchar(200),
+    grade         varchar(50),
+    about_me      TEXT,
+    with_me       TEXT,
+    member_status varchar(50),
+    modified_at   timestamp,
+    created_at    timestamp,
 
-    fk_location_info_id bigint,
-
-    constraint pk_member_id primary key (id)
+    constraint pk_member_id primary key (id),
+    constraint uk_member_email unique (email),
+    constraint uk_member_nick_name unique (nick_name)
 );
 
-create table studygroup_join (
-  id            bigint auto_increment,
-  member_id     bigint,
-  studygroup_id bigint,
-  approve       boolean,
-
-  constraint pk_studygroup_join_id primary key (id)
+create table if not exists member_roles
+(
+    member_id bigint not null,
+    roles     varchar(30)
 );
 
-create table studygroup (
-    id                bigint auto_increment,
-    study_name        varchar (50),
-    introduction      TEXT,
-    maxClassmateCount int,
-    platform          varchar(50),
-    start_day         timestamp,
-    end_day           timestamp,
-    modified_at       timestamp,
-    created_at        timestamp,
-    member_id         bigint,
+create table if not exists studygroup_join
+(
+    id            bigint auto_increment,
+    is_approved   boolean default false,
+
+    member_id     bigint,
+    studygroup_id bigint,
+
+    constraint pk_studygroup_join_id primary key (id)
+);
+
+create table if not exists studygroup
+(
+    id                     bigint auto_increment,
+    study_name             varchar(50),
+    studygroup_image       blob,
+    address                varchar(200),
+    study_period           varchar(100),
+    study_time             varchar(100),
+    introduction           TEXT,
+    member_count_min       int,
+    member_count_max       int,
+    platform               varchar(50),
+    modified_at            timestamp,
+    created_at             timestamp,
+
+    leader_member_id       bigint,
+    calendar_studygroup_id bigint,
 
     constraint pk_studygroup_id primary key (id)
 );
 
-create table study_post_comment
+create table if not exists search_tag
+(
+    id            bigint auto_increment,
+    tag_key       varchar(20),
+    tag_value     varchar(100),
+
+    studygroup_id bigint,
+
+    constraint pk_search_tag_id primary key (id)
+);
+
+create table if not exists study_post_comment
 (
     id            bigint auto_increment,
     content       varchar(200),
     modified_at   timestamp,
     created_at    timestamp,
-    member_id     bigint,
+
     studygroup_id bigint,
+    member_id     bigint,
 
     constraint pk_study_post_comment_id primary key (id)
 );
 
-create table search_tag (
+create table if not exists time_schedule
+(
     id            bigint auto_increment,
-    studygroup_id bigint,
-    tag_key       varchar(20),
-    tag_value     varchar(100),
-
-    constraint pk_search_tag_id primary key (id)
-);
-
-create table time_schedule (
-    id            bigint auto_increment,
-    studygroup_id bigint,
-    day           varchar(10),
     start_time    timestamp,
     end_time      timestamp,
+
+    member_id     bigint,
+    studygroup_id bigint,
 
     constraint pk_time_schedule_id primary key (id)
 );
 
-create table private_time_schedule (
-    id         bigint auto_increment,
-    member_id  bigint,
-    title      varchar(100),
-    day        varchar(10),
-    start_time timestamp,
-    end_time   timestamp,
-
-    constraint pk_private_time_schedule_id primary key (id)
-);
-
-create table member_location (
-    id        bigint auto_increment,
-    address   varchar(200),
-    latitude  float,
-    longitude float,
-    member_id bigint,
-
-    constraint pk_member_location_id primary key (id)
-);
-
-create table study_location (
-    id            bigint auto_increment,
-    address       varchar(200),
-    latitude      float,
-    longitude     float,
-    studygroup_id bigint,
-
-    constraint pk_study_location_id primary key (id)
-);
+alter table member_roles
+    add constraint fk_member_roles_member_id
+        foreign key (member_id) references member (id);
 
 alter table study_post_comment
-    add constraint fk_member_id
+    add constraint fk_study_post_comment_member_id
         foreign key (member_id) references member (id);
 alter table study_post_comment
-    add constraint fk_studygroup_id
+    add constraint fk_study_post_comment_studygroup_id
         foreign key (studygroup_id) references studygroup (id);
+
 alter table studygroup_join
-    add constraint fk_studygroup_id
-        foreign key (studygroup_id) references studygroup (id);
-alter table studygroup_join
-    add constraint fk_member_id
+    add constraint fk_studygroup_join_member_id
         foreign key (member_id) references member (id);
-alter table studygroup
-    add constraint fk_member_id
+alter table studygroup_join
+    add constraint fk_studygroup_join_studygroup_id
+        foreign key (studygroup_id) references studygroup (id);
+
+alter table search_tag
+    add constraint fk_search_tag_studygroup_id
+        foreign key (studygroup_id) references studygroup (id);
+
+alter table time_schedule
+    add constraint fk_time_schedule_member_id
         foreign key (member_id) references member (id);
 alter table time_schedule
-    add constraint fk_studygroup_id
+    add constraint fk_time_schedule_studygroup_id
         foreign key (studygroup_id) references studygroup (id);
-alter table search_tag
-    add constraint fk_studygroup_id
-        foreign key (studygroup_id) references studygroup (id);
-alter table member_location
-    add constraint fk_member_id
-        foreign key (member_id) references member (id);
-alter table study_location
-    add constraint fk_studygroup_id
-        foreign key (studygroup_id) references studygroup (id);
-alter table private_time_schedule
-    add constraint fk_member_id
-        foreign key (member_id) references member (id);
+
+alter table studygroup
+    add constraint fk_studygroup_leader_member_id
+        foreign key (leader_member_id) references member (id);
