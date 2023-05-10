97828e0ca2a81a90e35bed95947b14d18f7472db
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 18:31:36 +0900

    feat: Member 엔티티의 studygroupsAsLeader, studygroupJoins, timeSchedules, studygroupPostComments 양방향 매핑 (리펙터링 필요)

--- server/edusync/src/main/java/com/codestates/edusync/member/entity/Member.java
+++ server/edusync/src/main/java/com/codestates/edusync/member/entity/Member.java
@@ -75,4 +75,92 @@ public class Member extends Auditable {
 
     @OneToMany(mappedBy = "member", cascade = {PERSIST, MERGE, REMOVE}, fetch = LAZY)
     private List<StudygroupPostComment> studygroupPostComments = new ArrayList<>();
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param studygroupsAsLeader  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setStudygroupsAsLeader(List<Studygroup> studygroupsAsLeader) {
+        if (studygroupsAsLeader == null) {
+            throw new IllegalArgumentException("studygroup As Leader List cannot be null");
+        }
+
+        if (this.studygroupsAsLeader != null) {
+            for (Studygroup studygroup : this.studygroupsAsLeader) {
+                studygroup.setLeaderMemberOneWay(null);
+            }
+        }
+
+        this.studygroupsAsLeader = studygroupsAsLeader;
+        for (Studygroup studygroup : this.studygroupsAsLeader) {
+            studygroup.setLeaderMemberOneWay(this);
+        }
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param studygroupJoins  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setStudygroupJoin(List<StudygroupJoin> studygroupJoins) {
+        if (studygroupJoins == null) {
+            throw new IllegalArgumentException("Studygroup Join List cannot be null");
+        }
+
+        if (this.studygroupJoins != null) {
+            for (StudygroupJoin studygroupJoin : this.studygroupJoins) {
+                studygroupJoin.setMemberOneWay(null);
+            }
+        }
+
+        this.studygroupJoins = studygroupJoins;
+        for (StudygroupJoin studygroupJoin : this.studygroupJoins) {
+            studygroupJoin.setMemberOneWay(this);
+        }
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param timeSchedules  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setTimeSchedule(List<TimeSchedule> timeSchedules) {
+        if (timeSchedules == null) {
+            throw new IllegalArgumentException("TimeSchedule List cannot be null");
+        }
+
+        if (this.timeSchedules != null) {
+            for (TimeSchedule timeSchedule : this.timeSchedules) {
+                timeSchedule.setMemberOneWay(null);
+            }
+        }
+
+        this.timeSchedules = timeSchedules;
+        for (TimeSchedule timeSchedule : this.timeSchedules) {
+            timeSchedule.setMemberOneWay(this);
+        }
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param studygroupPostComments  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setStudygroupPostComment(List<StudygroupPostComment> studygroupPostComments) {
+        if (studygroupPostComments == null) {
+            throw new IllegalArgumentException("StudygroupPostComment List cannot be null");
+        }
+
+        if (this.studygroupPostComments != null) {
+            for (StudygroupPostComment studygroupPostComment : this.studygroupPostComments) {
+                studygroupPostComment.setMemberOneWay(null);
+            }
+        }
+
+        this.studygroupPostComments = studygroupPostComments;
+        for (StudygroupPostComment studygroupPostComment : this.studygroupPostComments) {
+            studygroupPostComment.setMemberOneWay(this);
+        }
+    }
 }
