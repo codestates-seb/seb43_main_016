772715636c0e66f8757b76151f8796bf02dd7aaf
serveman <120380939+serveman@users.noreply.github.com>
2023-05-11 02:10:02 +0900

    fix: CalendarDto 수정

--- server/edusync/src/main/java/com/codestates/edusync/study/plancalendar/studygroup/dto/CalendarDto.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/plancalendar/studygroup/dto/CalendarDto.java
@@ -1,11 +1,11 @@
 package com.codestates.edusync.study.plancalendar.studygroup.dto;
 
-import com.codestates.edusync.infodto.timeschedule.entity.TimeSchedule;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class CalendarDto {
@@ -14,7 +14,7 @@ public class CalendarDto {
     @Getter
     public static class Post {
         @NotNull
-        private List<TimeSchedule> timeSchedules;
+        private List<TimeScheduleDto.Post> timeSchedules;
     }
 
     @NoArgsConstructor
@@ -22,6 +22,38 @@ public class CalendarDto {
     @Setter
     public static class Patch {
         @NotNull
-        private List<TimeSchedule> timeSchedules;
+        private List<TimeScheduleDto.Patch> timeSchedules;
+    }
+
+    public static class TimeScheduleDto {
+
+        @NoArgsConstructor
+        @Getter
+        @Setter
+        public static class Post {
+            @NotNull
+            private String title;
+
+            @NotNull
+            private String content;
+
+            @NotNull
+            private Timestamp start;
+
+            @NotNull
+            private Timestamp end;
+        }
+
+        @NoArgsConstructor
+        @Getter
+        @Setter
+        public static class Patch {
+            private Long id;
+            private String title;
+            private String content;
+            private Timestamp start;
+            private Timestamp end;
+        }
+
     }
 }
