5486ac6f49a381ac8d56da766ad72072328908c5
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 17:32:57 +0900

    feat: StudygroupJoin 엔티티의 Studygroup / Member 양방향 매핑

--- server/edusync/src/main/java/com/codestates/edusync/study/studygroupjoin/entity/StudygroupJoin.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroupjoin/entity/StudygroupJoin.java
@@ -7,11 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
-import static javax.persistence.FetchType.EAGER;
-import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.FetchType.*;
 
 @NoArgsConstructor
 @Getter
@@ -32,4 +29,59 @@ public class StudygroupJoin {
     @ManyToOne(fetch = EAGER)
     @JoinColumn(name = "studygroup_join_studygroup_id")
     private Studygroup studygroup;
+
+
+    /**
+     * <h2>양방향 매핑을 위한 메서드</h2>
+     * 기존 연결을 끊고, 새로 관계를 연결한다.<br>
+     * <font color="white"><b>양방향 매핑 시 순환참조 가능성이 있으므로, 반대쪽에서 사용하면 절대 안됨</b></color="white"><br>
+     * 참고: {@link StudygroupJoin#setStudygroupOneWay(Studygroup)}
+     * @param studygroup 양방향 매핑을 위한 인자
+     */
+    public void setStudygroup(Studygroup studygroup) {
+        if (studygroup == null) {
+            throw new IllegalArgumentException("Studygroup cannot be null");
+        }
+
+        if(this.studygroup != null) {
+            this.studygroup.getStudygroupJoins().remove(this);
+        }
+        this.studygroup = studygroup;
+        this.studygroup.getStudygroupJoins().add(this);
+    }
+
+    /**
+     * <h2>양방향 매핑 중 순환 방지용으로 단방향으로만 추가하는 메서드</h2>
+     * @param studygroup 관계를 끊는 경우 null 가능
+     */
+    public void setStudygroupOneWay(Studygroup studygroup) {
+        this.studygroup = studygroup;
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 메서드</h2>
+     * 기존 연결을 끊고, 새로 관계를 연결한다.<br>
+     * <font color="white"><b>양방향 매핑 시 순환참조 가능성이 있으므로, 반대쪽에서 사용하면 절대 안됨</b></color="white"><br>
+     * 참고: {@link StudygroupJoin#setStudygroupOneWay(Studygroup)}
+     * @param member 양방향 매핑을 위한 인자
+     */
+    public void setMember(Member member) {
+        if (member == null) {
+            throw new IllegalArgumentException("Member cannot be null");
+        }
+
+        if(this.member != null) {
+            this.member.getStudygroupJoins().remove(this);
+        }
+        this.member = member;
+        this.member.getStudygroupJoins().add(this);
+    }
+
+    /**
+     * <h2>양방향 매핑 중 순환 방지용으로 단방향으로만 추가하는 메서드</h2>
+     * @param member 관계를 끊는 경우 null 가능
+     */
+    public void setMemberOneWay(Member member) {
+        this.member = member;
+    }
 }
