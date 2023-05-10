5b22eff6bb509fa9ab8456402c7d9a0236efad11
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 16:49:33 +0900

    doc: Studygroup 엔티티의 leaderMember 양방향 매핑

--- server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
@@ -79,4 +79,53 @@ public class Studygroup extends Auditable {
 
     @OneToMany(mappedBy = "studygroup", cascade = {PERSIST, MERGE, REMOVE}, fetch = LAZY)
     private List<SearchTag> tags = new ArrayList<>();
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param timeSchedules  새로 매핑할 TimeSchedule 의 리스트( 시간표 )
+     */
+    public void setTimeSchedules(List<TimeSchedule> timeSchedules) {
+        if (timeSchedules == null) {
+            throw new IllegalArgumentException("TimeSchedule List cannot be null");
+        }
+
+        if (this.timeSchedules != null) {
+            for (TimeSchedule timeSchedule : this.timeSchedules) {
+                timeSchedule.setStudygroupOneWay(null);
+            }
+        }
+
+        this.timeSchedules = timeSchedules;
+        for (TimeSchedule timeSchedule : this.timeSchedules) {
+            timeSchedule.setStudygroupOneWay(this);
+        }
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 메서드</h2>
+     * 기존 연결을 끊고, 새로 관계를 연결한다.<br>
+     * <font color="white"><b>양방향 매핑 시 순환참조 가능성이 있으므로, 반대쪽에서 사용하면 절대 안됨</b></color="white"><br>
+     * 참고: {@link Studygroup#setLeaderMemberOneWay(Member)}
+     * @param leaderMember
+     */
+    public void setLeaderMember(Member leaderMember) {
+        if (leaderMember == null) {
+            throw new IllegalArgumentException("Leader Member cannot be null");
+        }
+
+        if(this.leaderMember != null) {
+            this.leaderMember.getStudygroupsAsLeader().remove(this);
+        }
+        this.leaderMember = leaderMember;
+        this.leaderMember.getStudygroupsAsLeader().add(this);
+    }
+
+    /**
+     * <h2>양방향 매핑 중 순환 방지용으로 단방향으로만 추가하는 메서드</h2>
+     * @param leaderMember 관계를 끊는 경우 null 가능
+     */
+    public void setLeaderMemberOneWay(Member leaderMember) {
+        this.leaderMember = leaderMember;
+    }
 }
