58d4ceda876ca608aa8d9bf9323a5f6b7884d9eb
serveman <120380939+serveman@users.noreply.github.com>
2023-05-10 19:41:01 +0900

    Be feat/schema - 엔티티에 schema 수정 내용 적용 (#103)
    * feat: schema.sql 추가, application.yml 의 ddl-auto 주석처리
    
    * remove: 임시로 추가했던 코드 삭제
    
    * feat: 기본 스키마 구성
    
    * feat: 두번째 버전 저장용
    
    * feat: 세번째 버전 스키마 (대폭 다이어트한 버전)
    
    * feat: Member 엔티티 설정
    
    * feat: TimeSchedule 엔티티 설정
    
    * feat: SearchTag 엔티티 설정
    
    * feat: StudygroupJoin 엔티티 설정
    remove: classmate 엔티티 삭제, StudygroupJoin Dto, Manager 삭제
    move: classmate 폴더 -> studygroupJoin
    
    * rename: classmate -> StudygroupJoin 변경
    
    * fix: member ~ StudygroupJoin ~ Studygroup 연관관계 수정
    
    * fix: Studygroup 캘린더 삭제
    
    * fix: StudygroupPostComment 컬럼명 변경
    
    * remove: Calendar 엔티티 삭제
    
    * fix: StudygroupDto 수정( 기간, 시간 -> String 으로 )
    
    * fix: TimeSchedule 입출력 형식 수정( 시:분 -> 월-일 시:분 )
    
    * fix: TimeScheduleResponseDto calendarInfo 없어짐에 따라 이너클래스로 변경
    
    * fix: 매핑 오류 수정
    
    * feat: TimeSchedule 엔티티 - 객체 양방향 관계 추가
    
    * fix: StudygroupJoin 엔티티의 양방향 관계 오류 수정
    
    * feat: 컬럼(필드) 추가
    
    * remove: 환경설정( ddl auto 제거 )
    
    * feat: TimeSchedule 엔티티에 schema 에 추가한 title, content 컬럼 적용
    
    * feat: Studygroup 엔티티에 schema 에 추가한 daysOfWeek, studyPeriod Start,End, studyTime Start,End 컬럼 적용
    
    * doc: TimeSchedule 엔티티의 양방향 매핑 메서드에 대한 자바독 추가
    
    * doc: Studygroup 엔티티의 leaderMember 양방향 매핑
    
    * feat: StudygroupJoin 엔티티의 Studygroup / Member 양방향 매핑
    
    * feat: StudygroupPostComment 엔티티의 Studygroup / Member 양방향 매핑
    
    * doc: 주석 변경
    
    * feat: SearchTag 엔티티의 Studygroup 양방향 매핑
    
    * feat: Studygroup 엔티티의 timeSchedules, leaderMember, StudygroupJoins, studygroupPostComments, searchTags 양방향 매핑
    
    * feat: Member 엔티티의 studygroupsAsLeader, studygroupJoins, timeSchedules, studygroupPostComments 양방향 매핑 (리펙터링 필요)

--- server/edusync/src/main/java/com/codestates/edusync/searchtag/entity/SearchTag.java
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/entity/SearchTag.java
@@ -28,4 +28,32 @@ public class SearchTag {
     @ManyToOne(fetch = LAZY, cascade = {PERSIST, MERGE, REMOVE})
     @JoinColumn(name = "studygroup_id")
     private Studygroup studygroup;
+
+
+    /**
+     * <h2>양방향 매핑을 위한 메서드</h2>
+     * 기존 연결을 끊고, 새로 관계를 연결한다.<br>
+     * <font color="white"><b>양방향 매핑 시 순환참조 가능성이 있으므로, 반대쪽에서 사용하면 절대 안됨</b></color="white"><br>
+     * 참고: {@link SearchTag#setStudygroupOneWay(Studygroup)}
+     * @param studygroup    양방향 매핑을 위한 객체
+     */
+    public void setStudygroup(Studygroup studygroup) {
+        if (studygroup == null) {
+            throw new IllegalArgumentException("Studygroup cannot be null");
+        }
+
+        if(this.studygroup != null) {
+            this.studygroup.getSearchTags().remove(this);
+        }
+        this.studygroup = studygroup;
+        this.studygroup.getSearchTags().add(this);
+    }
+
+    /**
+     * <h2>양방향 매핑 중 순환 방지용으로 단방향으로만 추가하는 메서드</h2>
+     * @param studygroup 관계를 끊는 경우 null 가능
+     */
+    public void setStudygroupOneWay(Studygroup studygroup) {
+        this.studygroup = studygroup;
+    }
 }
--- server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroup/entity/Studygroup.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,11 +37,20 @@ public class Studygroup extends Auditable {
     @Column(length = 200)
     private String address;
 
-    @Column(length = 100, name = "study_period")
-    private String studyPeriod;
+    @Column(length = 50, name = "days_of_week")
+    private String daysOfWeek;
 
-    @Column(length = 100, name = "study_time")
-    private String studyTime;
+    @Column(name = "study_period_start")
+    private Timestamp studyPeriodStart;
+
+    @Column(name = "study_period_end")
+    private Timestamp studyPeriodEnd;
+
+    @Column(name = "study_time_start")
+    private Timestamp studyTimeStart;
+
+    @Column(name = "study_time_end")
+    private Timestamp studyTimeEnd;
 
     @OneToMany(mappedBy = "studygroup", fetch = LAZY)
     private List<TimeSchedule> timeSchedules = new ArrayList<>();
@@ -68,5 +78,123 @@ public class Studygroup extends Auditable {
     private List<StudygroupPostComment> studygroupPostComments = new ArrayList<>();
 
     @OneToMany(mappedBy = "studygroup", cascade = {PERSIST, MERGE, REMOVE}, fetch = LAZY)
-    private List<SearchTag> tags = new ArrayList<>();
+    private List<SearchTag> searchTags = new ArrayList<>();
+
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param timeSchedules  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setTimeSchedules(List<TimeSchedule> timeSchedules) {
+        if (timeSchedules == null) {
+            throw new IllegalArgumentException("TimeSchedule List cannot be null");
+        }
+
+        if (this.timeSchedules != null) {
+            for (TimeSchedule timeSchedule : this.timeSchedules) {
+                timeSchedule.setStudygroupOneWay(null);
+            }
+        }
+
+        this.timeSchedules = timeSchedules;
+        for (TimeSchedule timeSchedule : this.timeSchedules) {
+            timeSchedule.setStudygroupOneWay(this);
+        }
+    }
+
+
+    /**
+     * <h2>양방향 매핑을 위한 메서드</h2>
+     * 기존 연결을 끊고, 새로 관계를 연결한다.<br>
+     * <font color="white"><b>양방향 매핑 시 순환참조 가능성이 있으므로, 반대쪽에서 사용하면 절대 안됨</b></color="white"><br>
+     * 참고: {@link Studygroup#setLeaderMemberOneWay(Member)}
+     * @param leaderMember  양방향 매핑을 위한 객체
+     */
+    public void setLeaderMember(Member leaderMember) {
+        if (leaderMember == null) {
+            throw new IllegalArgumentException("Leader Member cannot be null");
+        }
+
+        if(this.leaderMember != null) {
+            this.leaderMember.getStudygroupsAsLeader().remove(this);
+        }
+        this.leaderMember = leaderMember;
+        this.leaderMember.getStudygroupsAsLeader().add(this);
+    }
+
+    /**
+     * <h2>양방향 매핑 중 순환 방지용으로 단방향으로만 추가하는 메서드</h2>
+     * @param leaderMember 관계를 끊는 경우 null 가능
+     */
+    public void setLeaderMemberOneWay(Member leaderMember) {
+        this.leaderMember = leaderMember;
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param studygroupJoins  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setStudygroupJoins(List<StudygroupJoin> studygroupJoins) {
+        if (studygroupJoins == null) {
+            throw new IllegalArgumentException("StudygroupJoin List cannot be null");
+        }
+
+        if (this.studygroupJoins != null) {
+            for (StudygroupJoin studygroupJoin : this.studygroupJoins) {
+                studygroupJoin.setStudygroupOneWay(null);
+            }
+        }
+
+        this.studygroupJoins = studygroupJoins;
+        for (StudygroupJoin studygroupJoin : this.studygroupJoins) {
+            studygroupJoin.setStudygroupOneWay(this);
+        }
+    }
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param studygroupPostComments  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setStudygroupPostComments(List<StudygroupPostComment> studygroupPostComments) {
+        if (studygroupPostComments == null) {
+            throw new IllegalArgumentException("StudygroupPostComment List cannot be null");
+        }
+
+        if (this.studygroupPostComments != null) {
+            for (StudygroupPostComment studygroupPostComment : this.studygroupPostComments) {
+                studygroupPostComment.setStudygroupOneWay(null);
+            }
+        }
+
+        this.studygroupPostComments = studygroupPostComments;
+        for (StudygroupPostComment studygroupPostComment : this.studygroupPostComments) {
+            studygroupPostComment.setStudygroupOneWay(this);
+        }
+    }
+
+
+    /**
+     * <h2>양방향 매핑을 위한 One To Many 쪽의 Setter 설정</h2>
+     * 기존에 연결되어있던 관계를 끊고, 양쪽 객체를 새로 연결해준다.
+     * @param searchTags  새로 매핑할 객체의 리스트( 시간표 )
+     */
+    public void setSearchTags(List<SearchTag> searchTags) {
+        if (searchTags == null) {
+            throw new IllegalArgumentException("Search Tag List cannot be null");
+        }
+
+        if (this.searchTags != null) {
+            for (SearchTag searchTag : this.searchTags) {
+                searchTag.setStudygroupOneWay(null);
+            }
+        }
+
+        this.searchTags = searchTags;
+        for (SearchTag searchTag : this.searchTags) {
+            searchTag.setStudygroupOneWay(this);
+        }
+    }
 }
