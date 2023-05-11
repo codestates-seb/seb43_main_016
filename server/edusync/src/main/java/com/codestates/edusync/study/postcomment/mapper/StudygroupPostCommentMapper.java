ffbe1a4e2a3c99b3e936c536aa820e4e2f707352
serveman <120380939+serveman@users.noreply.github.com>
2023-05-11 21:48:40 +0900

    fix: StudygroupPostComment 버그 수정

--- server/edusync/src/main/java/com/codestates/edusync/study/postcomment/mapper/StudygroupPostCommentMapper.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/postcomment/mapper/StudygroupPostCommentMapper.java
@@ -3,8 +3,10 @@ package com.codestates.edusync.study.postcomment.mapper;
 import com.codestates.edusync.study.postcomment.dto.StudygroupPostCommentDto;
 import com.codestates.edusync.study.postcomment.dto.StudygroupPostCommentResponseDto;
 import com.codestates.edusync.study.postcomment.entity.StudygroupPostComment;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 
@@ -13,8 +15,12 @@ public interface StudygroupPostCommentMapper {
     StudygroupPostComment studygroupPostCommentPostDtoToStudygroupPostComment(StudygroupPostCommentDto.Post postDto);
     StudygroupPostComment studygroupPostCommentPatchDtoToStudygroupPostComment(StudygroupPostCommentDto.Patch patchDto);
 
+    @IterableMapping(qualifiedByName = "EntityToResponse")
+    List<StudygroupPostCommentResponseDto> studygroupPostCommentToStudygroupPostCommentResponseDtos(List<StudygroupPostComment> comment);
+
+    @Named("EntityToResponse")
     @Mapping(source = "id", target = "commentId")
     @Mapping(source = "studygroup.id", target = "studygroupId")
     @Mapping(source = "member.id", target = "memberId")
-    List<StudygroupPostCommentResponseDto> studygroupPostCommentToStudygroupPostCommentResponseDtos(List<StudygroupPostComment> comment);
+    StudygroupPostCommentResponseDto studygroupPostCommentToStudygroupPostCommentResponseDto(StudygroupPostComment comment);
 }
