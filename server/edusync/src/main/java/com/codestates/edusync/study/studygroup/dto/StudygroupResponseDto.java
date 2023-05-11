84e525c1d5ed8710f4e6a04d6de08aab1a7a3c00
yeori316 <78740368+yeori316@users.noreply.github.com>
2023-05-11 12:26:31 +0900

    [BE] Study 기본 기능 초기 설정 (#113)
    * feat: 스터디 등록 기능 초기 완료
    
    * feat: 스터디 삭제 기능 초기 완료
    StudygroupPostComment 테이블명이랑 매칭 필요
    
    * feat: 스터디 정보 수정 기능 초기 완료
    feat: 스터디 모집 상태 수정 기능 초기 완료
    
    * feat: 스터디 조회 기능 미완료
    
    * feat: 스터디 기능 초기 확인
    
    * Update server/edusync/src/main/java/com/codestates/edusync/study/studygroup/dto/StudygroupDto.java
    
    Co-authored-by: serveman <120380939+serveman@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: serveman <120380939+serveman@users.noreply.github.com>

--- server/edusync/src/main/java/com/codestates/edusync/study/studygroup/dto/StudygroupResponseDto.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroup/dto/StudygroupResponseDto.java
@@ -1,9 +1,16 @@
 package com.codestates.edusync.study.studygroup.dto;
 
+import com.codestates.edusync.searchtag.entity.SearchTag;
 import com.codestates.edusync.study.studygroupjoin.dto.ClassmateResponseDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.tomcat.jni.Time;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -11,23 +18,48 @@ import lombok.Setter;
 public class StudygroupResponseDto {
     private Long id;
     private String studyName;
-    private String calendar;
-    private String timeSchedule;
-    private String introduction;
-    private Integer maxClassmateCount;
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private Timestamp studyPeriodStart;
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    private Timestamp studyPeriodEnd;
+    private String daysOfWeek;
+    @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")
+    private Timestamp studyTimeStart;
+    @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")
+    private Timestamp studyTimeEnd;
+    private Integer memberCountMin;
+    private Integer memberCountMax;
     private String platform;
-    private String address;
-
-    private ClassmateResponseDto leader;
-    private Count count;
+    private String introduction;
+    private Boolean requited;
+    private List<SearchTag> tags;
+    private StudyLeader leader;
 
     @NoArgsConstructor
     @Getter
     @Setter
-    public static class Count {
-        private Integer waiter;
-        private Integer classmate;
-        private Integer comment;
-        private Integer tag;
+    public static class StudyLeader {
+        private Long id;
+        private String nickName;
     }
+
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class DtoList {
+        private Long id;
+        private String title;
+        private List<SearchTag> tags;
+    }
+//    private Count count;
+//
+//    @NoArgsConstructor
+//    @Getter
+//    @Setter
+//    public static class Count {
+//        private Integer waiter;
+//        private Integer classmate;
+//        private Integer comment;
+//        private Integer tag;
+//    }
 }
