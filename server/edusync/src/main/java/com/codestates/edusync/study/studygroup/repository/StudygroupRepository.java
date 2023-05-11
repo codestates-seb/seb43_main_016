9f7056776463550b7d06a7ba33c9d92a754401b7
yeori316 <78740368+yeori316@users.noreply.github.com>
2023-05-11 23:17:06 +0900

    Update server/edusync/src/main/java/com/codestates/edusync/study/studygroup/repository/StudygroupRepository.java
    Co-authored-by: serveman <120380939+serveman@users.noreply.github.com>

--- server/edusync/src/main/java/com/codestates/edusync/study/studygroup/repository/StudygroupRepository.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/studygroup/repository/StudygroupRepository.java
@@ -2,7 +2,6 @@ package com.codestates.edusync.study.studygroup.repository;
 
 import com.codestates.edusync.study.studygroup.entity.Studygroup;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface StudygroupRepository extends JpaRepository<Studygroup, Long> {
 
