0caf279e2355a1a0c3a15d73daf198c30c8849bc
serveman <120380939+serveman@users.noreply.github.com>
2023-05-12 00:54:06 +0900

    feat: SearchTagManager 변경

--- server/edusync/src/main/java/com/codestates/edusync/searchtag/utils/SearchTagManager.java
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/utils/SearchTagManager.java
@@ -9,6 +9,15 @@ public interface SearchTagManager {
      * <h2>key(카테고리)를 받아 해당 key 에 대한 value(태그)의 리스트를 반환해준다</h2>
      * <s><font color="white"><b> 403 forbidden </b></font> 로그인을 하지 않은 경우</s><br>
      * <font color="white"><b> 404 not found </b></font> 해당 key 에 대한 값이 하나도 존재하지 않을 경우<br>
+     * @param studygroupId   검색에 사용할 스터디 그룹의 식별자
+     * @return
+     */
+    List<SearchTag> getSearchTagList(Long studygroupId);
+
+    /**
+     * <h2>key(카테고리)를 받아 해당 key 에 대한 value(태그)의 리스트를 반환해준다</h2>
+     * <s><font color="white"><b> 403 forbidden </b></font> 로그인을 하지 않은 경우</s><br>
+     * <font color="white"><b> 404 not found </b></font> 해당 key 에 대한 값이 하나도 존재하지 않을 경우<br>
      * @param key   검색에 사용할 key(카테고리)
      * @return      key(카테고리)에 해당하는 value(태그)의 목록 전부
      */
@@ -18,7 +27,6 @@ public interface SearchTagManager {
      * <h2>모든 key(카테고리)와 value(태그)의 쌍을 리스트로 반환해준다</h2>
      * <s><font color="white"><b> 403 forbidden </b></font> 로그인을 하지 않은 경우</s><br>
      * <font color="white"><b> 404 not found </b></font> 해당 key 에 대한 값이 하나도 존재하지 않을 경우<br>
-     * @param key   검색에 사용할 key(카테고리)
      * @return      key(카테고리)에 해당하는 value(태그)의 목록 전부
      */
     List<SearchTag> getAllSearchTagList();
@@ -37,4 +45,13 @@ public interface SearchTagManager {
      * @param tags
      */
     void deleteSearchTags(List<SearchTag> tags);
+
+    /**
+     * <h2>그룹의 식별자를 입력받아, DB 에서 삭제한다</h2>
+     * <font color="white"><b> 403 forbidden </b></font> 로그인을 하지 않은 경우<br>
+     * <font color="white"><b> 403 forbidden </b></font> 해당 스터디의 리더가 아닌 경우<br>
+     * <font color="white"><b> 404 not found </b></font> 스터디 그룹이 존재하지 않는 경우<br>
+     * @param studygroupId
+     */
+    void deleteSearchTags(Long studygroupId);
 }
