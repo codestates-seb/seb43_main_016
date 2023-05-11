ad95d87dbff1b68c318c0bfa941eea332551a5f6
serveman <120380939+serveman@users.noreply.github.com>
2023-05-12 02:16:51 +0900

    feat: SearchTag 엔티티로 받던 데이터를 해시맵으로 변경

--- server/edusync/src/main/java/com/codestates/edusync/searchtag/dto/SearchTagDto.java
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/dto/SearchTagDto.java
@@ -3,13 +3,17 @@ package com.codestates.edusync.searchtag.dto;
 import com.codestates.edusync.searchtag.entity.SearchTag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import java.util.HashMap;
 import java.util.List;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class SearchTagDto {
+    private Long studygroupId;
     @NotNull
-    private List<SearchTag> tags;
+    private HashMap<String, String> tags;
 }
