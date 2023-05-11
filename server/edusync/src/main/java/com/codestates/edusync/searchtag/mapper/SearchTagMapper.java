898c4515e69f57c7baa741f778b2f1ff60993e79
serveman <120380939+serveman@users.noreply.github.com>
2023-05-12 04:24:24 +0900

    [구현] 태그 검색 기능 추가 (#130)
    * feat: SearchTagService 기능 추가 ( 스터디 그룹 식별자로 삭제, 조회 )
    
    * feat: SearchTagManager 변경
    
    * feat: SearchTagDto 신규 추가 ( Post, Patch 공용 )
    
    * feat: SearchTagMapper 추가
    
    * feat: SearchTagRepository 추가
    
    * feat: SearchTagManager 추가
    
    * feat: SearchTagService Update 추가
    
    * feat: SearchTagController 추가 (스터디id로 조회, 리스트 등록, 스터디idfh 수정, 리스트 삭제
    
    * feat: 컨트롤러 변경에 따라 나머지도 변경
    
    * feat: SearchTag 엔티티로 받던 데이터를 해시맵으로 변경
    
    * feat: SearchTag 엔티티로 받던 데이터를 해시맵으로 변경
    
    * feat: SearchTag 엔티티로 삭제 -> 키+값 삭제로 변경 ( 엔티티 자체가 일치하지않아서 삭제 안되기에 변경 )

--- server/edusync/src/main/java/com/codestates/edusync/searchtag/mapper/SearchTagMapper.java
+++ server/edusync/src/main/java/com/codestates/edusync/searchtag/mapper/SearchTagMapper.java
@@ -1,9 +1,12 @@
 package com.codestates.edusync.searchtag.mapper;
 
+import com.codestates.edusync.searchtag.dto.SearchTagDto;
 import com.codestates.edusync.searchtag.dto.SearchTagResponseDto;
 import com.codestates.edusync.searchtag.entity.SearchTag;
+import com.codestates.edusync.study.studygroup.entity.Studygroup;
 import org.mapstruct.Mapper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,6 +19,24 @@ public interface SearchTagMapper {
 
         tags.forEach(tag -> resultTags.put(tag.getTagKey(), tag.getTagValue()));
         result.setTags(resultTags);
+
+        return result;
+    }
+
+    default List<SearchTag> searchTagDtoToSearchTags(SearchTagDto postDto) {
+        Studygroup studygroup = new Studygroup();
+        studygroup.setId(postDto.getStudygroupId());
+
+        List<SearchTag> result = new ArrayList<>();
+        postDto.getTags()
+                .forEach((key, value) -> {
+                    SearchTag resultTag = new SearchTag();
+                    resultTag.setTagKey(key);
+                    resultTag.setTagValue(value);
+                    resultTag.setStudygroup(studygroup);
+
+                    result.add(resultTag);
+                });
 
         return result;
     }
