485dce3a6470390e6ba258e34857f1b0a03f2c95
wish9 <wjwee9@gmail.com>
2023-05-12 19:02:28 +0900

    fix : oauth2 로그인 리다이렉트 주소 변경 (프론트 배포주소로 변경)

--- server/edusync/src/main/java/com/codestates/edusync/member/service/MemberService.java
+++ server/edusync/src/main/java/com/codestates/edusync/member/service/MemberService.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @AllArgsConstructor
 @Transactional
