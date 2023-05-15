84c0c5e711dea06e75e3e5554503704b31bfa66a
serveman <120380939+serveman@users.noreply.github.com>
2023-05-15 16:54:09 +0900

    feat: StudygroupPostCommentService 에서 검증 로직을 유틸로 이동

--- server/edusync/src/main/java/com/codestates/edusync/study/postcomment/service/StudygroupPostCommentService.java
+++ server/edusync/src/main/java/com/codestates/edusync/study/postcomment/service/StudygroupPostCommentService.java
@@ -1,6 +1,7 @@
 package com.codestates.edusync.study.postcomment.service;
 
 import com.codestates.edusync.exception.BusinessLogicException;
+import com.codestates.edusync.globalutils.VerifyStudygroupPostCommentUtils;
 import com.codestates.edusync.member.entity.Member;
 import com.codestates.edusync.member.service.MemberService;
 import com.codestates.edusync.study.postcomment.entity.StudygroupPostComment;
@@ -22,16 +23,17 @@ import static com.codestates.edusync.exception.ExceptionCode.*;
 @Service
 public class StudygroupPostCommentService implements StudygroupPostCommentManager {
     private final StudygroupPostCommentRepository studygroupPostCommentRepository;
-    private final MemberService memberService;
-    private final StudygroupService studygroupService;
+    private final VerifyStudygroupPostCommentUtils verifyStudygroupPostCommentUtils;
+    private final VerifyStudygroupUtils verifyStudygroupUtils;
+    private final VerifyMemberUtils verifyMemberUtils;
 
     @Override
     public StudygroupPostComment createStudygroupPostComment(Long studygroupId,
                                                              StudygroupPostComment comment) {
-        Member findMember = memberService.findVerifyMemberWhoLoggedIn();
+        Member findMember = verifyMemberUtils.findVerifyMemberWhoLoggedIn();
         comment.setMember(findMember);
 
-        Studygroup findStudygroup = studygroupService.findStudygroup(studygroupId);
+        Studygroup findStudygroup = verifyStudygroupUtils.findStudygroup(studygroupId);
         comment.setStudygroup(findStudygroup);
 
         return studygroupPostCommentRepository.save(comment);
@@ -40,56 +42,15 @@ public class StudygroupPostCommentService implements StudygroupPostCommentManage
     @Override
     public StudygroupPostComment updateStudygroupPostComment(Long studygroupId, Long commentId,
                                                              StudygroupPostComment patchComment) {
-        Member findMember = memberService.findVerifyMemberWhoLoggedIn();
-        StudygroupPostComment findComment = findVerifyStudygroupPostComment(commentId);
+        Member findMember = verifyMemberUtils.findVerifyMemberWhoLoggedIn();
+        StudygroupPostComment findComment = verifyStudygroupPostCommentUtils.findVerifyStudygroupPostComment(commentId);
         
-        verifyStudygroupPostComment(findMember.getId(), studygroupId, findComment);
+        verifyStudygroupPostCommentUtils.verifyStudygroupPostComment(findMember.getId(), studygroupId, findComment);
         
         Optional.ofNullable(patchComment.getContent())
                 .ifPresent(findComment::setContent);
         
         return studygroupPostCommentRepository.save(findComment);
-    }
-
-    /**
-     * <h2>댓글 조회</h2>
-     * 댓글 식별자에 해당하는 댓글이 존재하는지 확인<br>
-     * <font color=white>404 Not Found </font> 댓글이 존재하지 않음 !<br>
-     * @param commentId
-     * @return
-     */
-    private StudygroupPostComment findVerifyStudygroupPostComment(Long commentId) {
-        return studygroupPostCommentRepository.findById(commentId)
-                .orElseThrow( () ->
-                        new BusinessLogicException(STUDYGROUP_POST_COMMENT_NOT_FOUND)
-                );
-    }
-
-    /**
-     * <h2>댓글의 유효성 검증</h2>
-     * <font color=white>400 Not matched</font> 요청받은 스터디 모집글의 식별자와, 코멘트의 스터디 식별자가 일치하지 않음<br>
-     * <font color=white>403 Not Allowed</font> 스터디 장이 아니고, 본인이 쓴 글이 아닐 때는 forbidden<br>
-     * @param memberId
-     * @param studygroupId
-     * @param comment
-     */
-    private void verifyStudygroupPostComment(Long memberId, Long studygroupId,
-                                             StudygroupPostComment comment) {
-        if( comment.getStudygroup().getId() != studygroupId ) {
-            throw new BusinessLogicException(STUDYGROUP_POST_COMMENT_NOT_MATCHED);
-        }
-
-        if( comment.getStudygroup().getLeaderMember().getId() != memberId &&
-                                  comment.getMember().getId() != memberId ) {
-            throw new BusinessLogicException(STUDYGROUP_POST_COMMENT_NOT_ALLOWED);
-        }
-    }
-    
-    private void verifyStudygroupMemberLeader(Long memberId,
-                                              Studygroup studygroup) {
-        if( studygroup.getLeaderMember().getId() != memberId ) {
-            throw new BusinessLogicException(STUDYGROUP_POST_COMMENT_ALLOWED_ONLY_FOR_LEADER);
-        }
     }
 
     @Override
@@ -99,22 +60,21 @@ public class StudygroupPostCommentService implements StudygroupPostCommentManage
 
     @Override
     public void deleteStudygroupPostComment(Long studygroupId, Long commentId) {
-        Member findMember = memberService.findVerifyMemberWhoLoggedIn();
-        StudygroupPostComment findComment = findVerifyStudygroupPostComment(commentId);
+        Member findMember = verifyMemberUtils.findVerifyMemberWhoLoggedIn();
+        StudygroupPostComment findComment = verifyStudygroupPostCommentUtils.findVerifyStudygroupPostComment(commentId);
 
-        verifyStudygroupPostComment(findMember.getId(), studygroupId, findComment);
+        verifyStudygroupPostCommentUtils.verifyStudygroupPostComment(findMember.getId(), studygroupId, findComment);
 
         studygroupPostCommentRepository.delete(findComment);
     }
 
     @Override
     public void deleteAllStudygroupPostCommentByStudygroupId(Long studygroupId) {
-        Member findMember = memberService.findVerifyMemberWhoLoggedIn();
-        Studygroup findStudygroup = studygroupService.findStudygroup(studygroupId);
+        Member findMember = verifyMemberUtils.findVerifyMemberWhoLoggedIn();
+        Studygroup findStudygroup = verifyStudygroupUtils.findStudygroup(studygroupId);
 
-        verifyStudygroupMemberLeader(findMember.getId(), findStudygroup);
+        verifyStudygroupPostCommentUtils.verifyStudygroupMemberLeader(findMember.getId(), findStudygroup);
         
         studygroupPostCommentRepository.deleteAllByStudygroupId(studygroupId);
     }
-
 }
