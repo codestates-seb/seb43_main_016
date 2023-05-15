44f6f84926f1298234864da6c9af407174c98f2a
wish9 <wjwee9@gmail.com>
2023-05-15 20:53:05 +0900

    comment : 설명 주석 위치 구현체 -> 인터페이스 이동 (#168)
    * fix : 토큰값 Bearer 누락 수정
    
    * fix : member 기능 인터페이스화
    
    * comment : 설명 주석 위치 구현체 -> 인터페이스 이동

--- server/edusync/src/main/java/com/codestates/edusync/globalutils/MemberVerifiableUtils.java
+++ server/edusync/src/main/java/com/codestates/edusync/globalutils/MemberVerifiableUtils.java
@@ -17,12 +17,6 @@ import java.util.Optional;
 public class MemberVerifiableUtils implements MemberVerifiable {
     private final MemberRepository memberRepository;
 
-    /**
-     * <h2>email 을 이용해서 회원을 검색하는 메서드</h2>
-     * email 로 회원 검색 후 검증하여 리턴해준다.
-     * @param email
-     * @return 토큰정보와 일치하는 member
-     */
     @Override
     public Member findVerifiedMemberByEmail(String email) {
         Optional<Member> optionalMember =
@@ -36,24 +30,12 @@ public class MemberVerifiableUtils implements MemberVerifiable {
         return findMember;
     }
 
-    /**
-     * <h2> uuid를 이용해서 회원을 검색하는 메서드</h2>
-     * uuid 리스트로 회원 검색 후 검증하여 리턴해준다.
-     * @param uuids
-     * @return uuid 리스트와 일치하는 member 리스트
-     */
     @Override
     @Transactional(readOnly = true)
     public List<Member> findMembersByUUID(List<String> uuids) {
         return memberRepository.findByUuidIn(uuids);
     }
 
-    /**
-     * <h2> 현재 활동 가능한 회원인지 확인하는 메서드</h2>
-     * 회원정보(Member)를 입력받아 현재 활동중인지 확인
-     * 삭제, 휴먼 계정이면 오류를 throw
-     * @param member
-     */
     @Override
     public void verifyMemberIsActive(Member member) {
         if (member.getMemberStatus() != Member.MemberStatus.MEMBER_ACTIVE) {
@@ -62,12 +44,6 @@ public class MemberVerifiableUtils implements MemberVerifiable {
         }
     }
 
-    /**
-     * <h2>이미 존재하는 이메일인지에 대한 검증</h2>
-     * 중복 불가 unique, update 불가 속성인 email을 이용하여, 이미 존재하는 이메일인지를 검증<br>
-     * <font color=white>409 Conflict </font>
-     * @param email
-     */
     @Override
     public void verifyExistMemberByEmail(String email) {
         Optional<Member> member = memberRepository.findByEmail(email);
@@ -75,12 +51,6 @@ public class MemberVerifiableUtils implements MemberVerifiable {
             throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS, String.format("%s는 이미 가입한 이메일입니다.", email));
     }
 
-    /**
-     * <h2>이미 존재하는 닉네임인지에 대한 검증</h2>
-     * 중복 불가 unique 속성인 nickName을 이용하여, 이미 존재하는 nickName인지를 검증<br>
-     * <font color=white>409 Conflict </font>
-     * @param nickName
-     */
     @Override
     public void verifyExistMemberByNickName(String nickName) {
         Optional<Member> member = memberRepository.findByNickName(nickName);
@@ -88,12 +58,6 @@ public class MemberVerifiableUtils implements MemberVerifiable {
             throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS, String.format("%s는 이미 사용중인 닉네임입니다.", nickName));
     }
 
-    /**
-     * <h2>현재 로그인 된 사용자의 정보를 리턴해주는 메서드</h2>
-     * security context에서 인증 정보를 받아 해당 사용자의 member 객체를 리턴
-     * @param authentication
-     * @return 접속 중인 Member 의 정보
-     */
     public Member findVerifyMemberWhoLoggedIn(Authentication authentication) {
         String email = authentication.getPrincipal().toString();
 
