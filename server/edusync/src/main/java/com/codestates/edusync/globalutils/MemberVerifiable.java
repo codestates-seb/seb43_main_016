e18f9f59ac6be42e3ee3b90d59c419d9a8e93361
wish9 <wjwee9@gmail.com>
2023-05-15 20:36:05 +0900

    comment : 설명 주석 위치 구현체 -> 인터페이스 이동

--- server/edusync/src/main/java/com/codestates/edusync/globalutils/MemberVerifiable.java
+++ server/edusync/src/main/java/com/codestates/edusync/globalutils/MemberVerifiable.java
@@ -10,11 +10,51 @@ import java.util.List;
   상태 코드에 대한 설명 참조: https://www.rfc-editor.org/rfc/rfc7231
  */
 public interface MemberVerifiable {
+    /**
+     * <h2>email 을 이용해서 회원을 검색하는 메서드</h2>
+     * email 로 회원 검색 후 검증하여 리턴해준다.
+     * @param email
+     * @return 토큰정보와 일치하는 member
+     */
     Member findVerifiedMemberByEmail(String email);
-    List<Member> findMembersByUUID(List<String> uuids);
-    void verifyMemberIsActive(Member member);
-    void verifyExistMemberByEmail(String email);
-    void verifyExistMemberByNickName(String nickname);
 
+    /**
+     * <h2> uuid를 이용해서 회원을 검색하는 메서드</h2>
+     * uuid 리스트로 회원 검색 후 검증하여 리턴해준다.
+     * @param uuids
+     * @return uuid 리스트와 일치하는 member 리스트
+     */
+    List<Member> findMembersByUUID(List<String> uuids);
+
+    /**
+     * <h2> 현재 활동 가능한 회원인지 확인하는 메서드</h2>
+     * 회원정보(Member)를 입력받아 현재 활동중인지 확인
+     * 삭제, 휴먼 계정이면 오류를 throw
+     * @param member
+     */
+    void verifyMemberIsActive(Member member);
+
+    /**
+     * <h2>이미 존재하는 이메일인지에 대한 검증</h2>
+     * 중복 불가 unique, update 불가 속성인 email을 이용하여, 이미 존재하는 이메일인지를 검증<br>
+     * <font color=white>409 Conflict </font>
+     * @param email
+     */
+    void verifyExistMemberByEmail(String email);
+
+    /**
+     * <h2>이미 존재하는 닉네임인지에 대한 검증</h2>
+     * 중복 불가 unique 속성인 nickName을 이용하여, 이미 존재하는 nickName인지를 검증<br>
+     * <font color=white>409 Conflict </font>
+     * @param nickName
+     */
+    void verifyExistMemberByNickName(String nickName);
+
+    /**
+     * <h2>현재 로그인 된 사용자의 정보를 리턴해주는 메서드</h2>
+     * security context에서 인증 정보를 받아 해당 사용자의 member 객체를 리턴
+     * @param authentication
+     * @return 접속 중인 Member 의 정보
+     */
     Member findVerifyMemberWhoLoggedIn(Authentication authentication);
 }
