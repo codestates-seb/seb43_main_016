0c429172e77e0b3b4858b1850114efca2a717195
serveman <120380939+serveman@users.noreply.github.com>
2023-05-19 13:49:07 +0900

    refactor: 검증 로직 utils 로 이동
    [skip ci]

--- server/edusync/src/main/java/com/codestates/edusync/model/study/plancalendar/service/CalendarMemberService.java
+++ server/edusync/src/main/java/com/codestates/edusync/model/study/plancalendar/service/CalendarMemberService.java
@@ -1,10 +1,10 @@
 package com.codestates.edusync.model.study.plancalendar.service;
 
-import com.codestates.edusync.exception.BusinessLogicException;
 import com.codestates.edusync.model.common.entity.TimeRange;
 import com.codestates.edusync.model.common.utils.MemberUtils;
 import com.codestates.edusync.model.common.utils.VerifyCalendarUtils;
 import com.codestates.edusync.model.common.utils.VerifyStudygroupUtils;
+import com.codestates.edusync.model.common.utils.VerifyTimeScheduleUtils;
 import com.codestates.edusync.model.member.entity.Member;
 import com.codestates.edusync.model.study.plancalendar.entity.TimeSchedule;
 import com.codestates.edusync.model.study.plancalendar.repository.CalendarRepository;
@@ -15,22 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-import static com.codestates.edusync.exception.ExceptionCode.TIME_SCHEDULE_NOT_LINKED;
-import static com.codestates.edusync.exception.ExceptionCode.TIME_SCHEDULE_NOT_MATCHED_WITH_MEMBER;
-
 @Transactional
 @RequiredArgsConstructor
 @Service
 public class CalendarMemberService {
     private final CalendarRepository calendarRepository;
-    private final VerifyCalendarUtils verifyCalendarUtils;
-    private final VerifyStudygroupUtils verifyStudygroupUtils;
+    private final VerifyCalendarUtils calendarUtils;
+    private final VerifyStudygroupUtils studygroupUtils;
+    private final VerifyTimeScheduleUtils timeScheduleUtils;
     private final MemberUtils memberUtils;
 
 public void createTimeSchedulesExceptStudygroup(TimeSchedule timeSchedule,
                                                 String email) {
         Member loginMember = memberUtils.getLoggedIn(email);
-
         timeSchedule.setMember(loginMember);
 
         calendarRepository.save(timeSchedule);
@@ -39,21 +36,10 @@ public void createTimeSchedulesExceptStudygroup(TimeSchedule timeSchedule,
     public void updateTimeSchedule(Long timeScheduleId,
                                    TimeSchedule timeSchedule,
                                    String email) {
-        Member loginMember = memberUtils.getLoggedIn(email);
-        TimeSchedule findTimeSchedule = verifyCalendarUtils.findVerifyTimeSchedule(timeScheduleId);
-
-        // todo: 예외 리펙터링 필요
-        if( findTimeSchedule.getMember() == null ) {
-            throw new BusinessLogicException(TIME_SCHEDULE_NOT_LINKED);
-        }
-
-        if( !findTimeSchedule.getMember().getId().equals(loginMember.getId()) ) {
-            throw new BusinessLogicException(TIME_SCHEDULE_NOT_MATCHED_WITH_MEMBER);
-        }
-
-        if( findTimeSchedule.getStudygroup() != null ) {
-            throw new BusinessLogicException(TIME_SCHEDULE_NOT_MATCHED_WITH_MEMBER);
-        }
+        TimeSchedule findTimeSchedule =
+                timeScheduleUtils.findVerifyTimeScheduleWithMember(
+                        timeScheduleId, email
+                );
 
         Optional.ofNullable(timeSchedule.getTitle()).ifPresent(findTimeSchedule::setTitle);
         Optional.ofNullable(timeSchedule.getPlatform()).ifPresent(findTimeSchedule::setPlatform);
@@ -77,13 +63,15 @@ public void createTimeSchedulesExceptStudygroup(TimeSchedule timeSchedule,
 
     public TimeSchedule getSingleTimeScheduleByTimeScheduleId(Long timeScheduleId) {
 
-        return verifyCalendarUtils.findVerifyTimeSchedule(timeScheduleId);
+        return calendarUtils.findVerifyTimeSchedule(timeScheduleId);
     }
 
     public void deleteTimeScheduleByTimeScheduleId(Long timeScheduleId,
                                                    String email) {
-        Member loginMember = memberUtils.getLoggedIn(email);
-        TimeSchedule findTimeSchedule = verifyCalendarUtils.findVerifyTimeSchedule(timeScheduleId);
+        TimeSchedule findTimeSchedule =
+                timeScheduleUtils.findVerifyTimeScheduleWithMember(
+                        timeScheduleId, email
+                );
 
         calendarRepository.delete(findTimeSchedule);
     }
