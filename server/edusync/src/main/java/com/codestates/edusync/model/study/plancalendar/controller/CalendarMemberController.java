88ba2824bdc86a474eda279e27f733386ca701dc
serveman <120380939+serveman@users.noreply.github.com>
2023-05-19 11:24:18 +0900

    feat: CalenderMember 엔드포인트 수정 및 주석 추가
    추가
    
    [skip ci]

--- server/edusync/src/main/java/com/codestates/edusync/model/study/plancalendar/controller/CalendarMemberController.java
+++ server/edusync/src/main/java/com/codestates/edusync/model/study/plancalendar/controller/CalendarMemberController.java
@@ -6,7 +6,6 @@ import com.codestates.edusync.model.study.plancalendar.dto.TimeScheduleResponseD
 import com.codestates.edusync.model.study.plancalendar.mapper.CalendarMapper;
 import com.codestates.edusync.model.study.plancalendar.service.CalendarMemberService;
 import com.codestates.edusync.model.study.plancalendar.entity.TimeSchedule;
-import com.codestates.edusync.model.study.plancalendar.dto.CalendarStudygroupDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,9 +27,15 @@ public class CalendarMemberController {
 
     private static final String DEFAULT_MEMBER_URL = "/members";
 
-    @PostMapping("/{timeschedule-id}" + DEFAULT_MEMBER_URL)
-    public ResponseEntity postCalendarMember(@PathVariable("timeschedule-id") @Positive Long timeScheduleId,
-                                             @Valid @RequestBody CalendarMemberDto.Post postDto,
+    /**
+     * <h2>개인 캘린더 단일 생성</h2>
+     * 스터디에 속해 있지 않은 일정을 개인적으로 생성<br>
+     * @param postDto        생성할 단일 일정
+     * @param authentication
+     * @return 응답 코드
+     */
+    @PostMapping(DEFAULT_MEMBER_URL)
+    public ResponseEntity postCalendarMember(@Valid @RequestBody CalendarMemberDto.Post postDto,
                                              Authentication authentication) {
         calendarMemberService.createTimeSchedulesExceptStudygroup(
                 mapper.memberTimeSchedulePostDtoToTimeSchedule(postDto.getTimeSchedule()),
@@ -40,6 +45,15 @@ public class CalendarMemberController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * <h2>개인 캘린더 단일 수정</h2>
+     * 스터디의 일정은 수정 불가능.<br>
+     * 개인이 생성한 것만 수정 가능<br>
+     * @param timeScheduleId 수정할 일정
+     * @param patchDto       수정할 내용이 담긴 DTO
+     * @param authentication
+     * @return 응답 코드
+     */
     @PatchMapping("/{timeschedule-id}" + DEFAULT_MEMBER_URL)
     public ResponseEntity patchCalendarMember(@PathVariable("timeschedule-id") @Positive Long timeScheduleId,
                                               @Valid @RequestBody CalendarMemberDto.Patch patchDto,
@@ -53,9 +67,14 @@ public class CalendarMemberController {
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/{timeschedule-id}" + DEFAULT_MEMBER_URL)
-    public ResponseEntity getAllTimeScheduleOfMember(@PathVariable("timeschedule-id") @Positive Long timeScheduleId,
-                                                     Authentication authentication) {
+    /**
+     * <h2>개인(본인) 캘린더 리스트 조회</h2>
+     * 스터디의 리스트가 포함된, 개인의 일정을 조회
+     * @param authentication
+     * @return 일정표 리스트
+     */
+    @GetMapping(DEFAULT_MEMBER_URL)
+    public ResponseEntity getAllTimeScheduleOfMember(Authentication authentication) {
 
         List<TimeSchedule> findTimeSchedules =
                 calendarMemberService.getTimeSchedules(authentication.getPrincipal().toString());
@@ -66,7 +85,14 @@ public class CalendarMemberController {
         return new ResponseEntity<>(responseDtos, HttpStatus.OK);
     }
 
-    @GetMapping("/{timeschedule-id}")
+    /**
+     * <h2>개인 캘린더 단일 조회</h2>
+     * 상세 조회<br>
+     * 스터디의 일정과는 응답 형식은 같으나, 내용을 다르게 설정할 수도 있음<br>
+     * @param timeScheduleId 일정 시간표
+     * @return 상세 조회 내용
+     */
+    @GetMapping("/{timeschedule-id}" + DEFAULT_MEMBER_URL)
     public ResponseEntity getSingleTimeSchedule(@PathVariable("timeschedule-id") @Positive Long timeScheduleId) {
         TimeSchedule findTimeSchedule =
                 calendarMemberService.getSingleTimeScheduleByTimeScheduleId(
@@ -79,6 +105,14 @@ public class CalendarMemberController {
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
+    /**
+     * <h2>개인 캘린더 단일 삭제</h2>
+     * 스터디의 일정은 삭제 불가능<br>
+     * 개인이 생성한 것만 삭제 가능<br>
+     * @param timeScheduleId 일정 시간
+     * @param authentication
+     * @return 응답 코드
+     */
     @DeleteMapping("/{timeschedule-id}" + DEFAULT_MEMBER_URL)
     public ResponseEntity deleteCalendarMember(@PathVariable("timeschedule-id") @Positive Long timeScheduleId,
                                                Authentication authentication) {
