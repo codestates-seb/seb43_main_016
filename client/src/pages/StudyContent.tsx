d90940d6508414217516c0b114785526e2fc9f8c
KangHaSeong <shaawwert6044@gmail.com>
2023-05-24 23:40:16 +0900

    게시글 파싱 및 HTML 변환 처리

--- client/src/pages/StudyContent.tsx
+++ client/src/pages/StudyContent.tsx
@@ -34,12 +34,12 @@ const StudyContent = () => {
 
   const [fetching, setFetching] = useState(true);
   const [content, setContent] = useState<StudyInfoDto | null>(initialState);
-  const [renderedIntroduction, setRenderedIntroduction] = useState<string | null>(null);
   const { id } = useParams(); // App.tsx의 Route url에 :id로 명시하면 그걸 가져옴
   const parsedId = Number(id);
   const navigate = useNavigate();
   const isLoggedIn = useRecoilValue(LogInState);
   const isRecruiting = content?.isRecruited;
+  const convertIntroduction = `${content?.introduction}`;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -60,16 +60,6 @@ const StudyContent = () => {
     };
     fetchData();
   }, [parsedId]);
-
-  const handleIntroduction = (introduction: string) => {
-    const createMarkup = () => {
-      return { __html: introduction };
-    };
-
-    setRenderedIntroduction(introduction);
-  };
-
-
 
   const handleDeleteButton = async () => {
     try {
@@ -103,6 +93,10 @@ const StudyContent = () => {
       console.error("Error during POST request:", error);
     }
     // 이미 등록한 스터디입니다 어떻게?
+  };
+
+  const markUp = (convertIntroduction: string) => {
+    return { __html: convertIntroduction };
   };
 
   return (
@@ -141,7 +135,9 @@ const StudyContent = () => {
                 <div>플랫폼</div>
                 <span>{content?.platform}</span>
               </StudyContentInfo>
-              <StudyContentText>{`${content?.introduction}`}</StudyContentText>
+              <StudyContentText
+                dangerouslySetInnerHTML={markUp(convertIntroduction)}
+              ></StudyContentText>
               <StudyContentProfileWrapper>
                 <StudyContentProfile>
                   <div className="profile-name">{`${content?.leaderNickName}`}</div>
