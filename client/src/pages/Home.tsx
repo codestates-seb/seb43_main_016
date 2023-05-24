b683b116b74b791e6174eace2eb5297984f8a3b4
lain-alice <freya6829@gmail.com>
2023-05-25 01:13:20 +0900

    design: 홈 화면 애니메이션, 링크

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,26 @@
 import styled from "styled-components";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <HomeContainer>
-      <HomeTitle>
+      <TitlePage>
         <h1>
           스터디 통합 관리 앱, <span>EduSync</span>입니다.
         </h1>
         <p>어쩌구저쩌구 저쩌구어쩌구</p>
-      </HomeTitle>
-      <HomeButton>
-        <button>로그인 화면으로 이동</button>
-      </HomeButton>
+        <TitleButton>
+          <button onClick={() => navigate("/login")}>
+            로그인 화면으로 이동
+          </button>
+        </TitleButton>
+      </TitlePage>
+      <IntroPage>
+        <div>바보</div>
+        <div>멍청이</div>
+      </IntroPage>
     </HomeContainer>
   );
 };
@@ -25,9 +33,10 @@ export default Home;
 
 const HomeContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background-color: #0093e9;
   background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+  /* background: linear-gradient(to right, #EAECC6, #2BC0E4); */
 
   display: flex;
   flex-direction: column;
@@ -35,7 +44,9 @@ const HomeContainer = styled.div`
   align-items: center;
 `;
 
-const HomeTitle = styled.div`
+const TitlePage = styled.div`
+  width: 100%;
+  height: 900px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -45,27 +56,52 @@ const HomeTitle = styled.div`
     font-size: 48px;
     color: #fff;
     margin-bottom: 30px;
+    animation: fadein 2.4s;
   }
   h1 > span {
-    color: #9be3ff;
+    color: #77ffef;
   }
   p {
     color: #fff;
     font-size: 16px;
     margin-bottom: 30px;
   }
+
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 `;
 
-const HomeButton = styled.div`
+const IntroPage = styled.div`
+  width: 100%;
+  height: 900px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  button {
+  > button {
     border: solid 3px #fff;
-    border-radius: none;
+    border-radius: 0;
     background-color: transparent;
+    padding: 7px 12px;
     color: #fff;
-    font-size: 16px;
+    font-size: 20px;
+    font-weight: 700;
+  }
+  > button:hover {
+    color: #0093e9;
+    background-color: #fff;
+    transition: all ease-in 0.3s;
   }
 `;
