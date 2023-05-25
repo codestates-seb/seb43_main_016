c66bcdf17ed8694846fc7af353981fde66b2d555
송하은 <songhaeun@songha-eun-ui-MacBookAir.local>
2023-05-25 15:35:29 +0900

    feat: 리스트 필터 목업 & gnb 메뉴 추가

--- client/src/components/gnb/GNB.tsx
+++ client/src/components/gnb/GNB.tsx
@@ -55,6 +55,9 @@ const GNB = () => {
             <Link to="/calendar">
               <span>나의 캘린더</span>
             </Link>
+            <Link to="/profile">
+              <span>나의 프로필</span>
+            </Link>
           </GNBMenuBlock>
           <User
             profileImage={profileImage}
--- client/src/pages/StudyList.tsx
+++ client/src/pages/StudyList.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import StudyListTag from "../components/StudyListTag";
 import studyImage from "../assets/studyImage.webp";
+import ListFilter from "../components/ListFilter";
 
 const StudyList = () => {
   interface StudyListDto {
@@ -43,7 +44,12 @@ const StudyList = () => {
     <StudyListContainer>
       <StudyListBody>
         <StudyListTop>
-          <h2>여러분의 스터디를 만들어보세요!</h2>
+          <div>
+            <h2>여러분의 스터디를 만들어보세요!</h2>
+            <div>
+              <ListFilter />
+            </div>
+          </div>
           <Link to="/studypost">
             <StudyPostButton>스터디 모집!</StudyPostButton>
           </Link>
@@ -102,13 +108,16 @@ const StudyListTop = styled.div`
   margin-bottom: 30px;
   justify-content: space-between;
   align-items: center;
-
+  div {
+    text-align: start;
+    margin-left: 10px;
+  }
   h2 {
     text-align: left;
     font-size: 2rem;
     color: #1f1f1f;
     font-weight: 700;
-    margin-left: 20px;
+    margin-left: 10px;
   }
 `;
 
