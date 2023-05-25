4dd2db2433e4ef0465c69679404ca8088a311624
송하은 <songhaeun@songha-eun-ui-MacBookAir.local>
2023-05-26 02:36:54 +0900

    fix: 자기소개, 동료 텍스트 입력 없으면 빈 텍스트가 들어감

--- client/src/pages/ProfileInfo.tsx
+++ client/src/pages/ProfileInfo.tsx
@@ -42,6 +42,7 @@ const ProfileInfo = () => {
       try {
         const info = await getMemberInfo(isLoggedIn);
         setMemberInfo(info);
+        setIntroduceInfo({ aboutMe: info.aboutMe, withMe: info.withMe });
       } catch (error) {
         //alert("로그인이 필요합니다.");
         console.error(error);
@@ -72,7 +73,7 @@ const ProfileInfo = () => {
       ...prevIntroduceInfo,
       [name]: value,
     }));
-    console.log(introduceInfo);
+    console.log("?", introduceInfo);
   };
 
   // TODO Save 버튼을 클릭 시, 유저의 자기소개 및 원하는 동료상을 서버에 PATCH하는 코드
